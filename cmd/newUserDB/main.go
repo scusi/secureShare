892471b24ad788028e7ff9746211b4339b4530a0
Florian Walther <florian.walther@posteo.de>
2018-05-16 14:07:23 +0200

    changed newUserDB cmd to reflect chenges in server user module

--- cmd/newUserDB/main.go
+++ cmd/newUserDB/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"github.com/cathalgarvey/go-minilock"
-	"github.com/scusi/secureShare/libs/user"
+	"github.com/scusi/secureShare/libs/server/user"
 	"log"
 )
 
@@ -15,19 +15,23 @@ var userPasswd string
 func init() {
 	flag.StringVar(&userName, "u", "", "username to be used")
 	flag.StringVar(&userPasswd, "p", "", "user password")
-	//userDB = new(user.UserDB)
+	userDB = new(user.UserDB)
 	/*
-	 */
-	userDB, err = user.LoadFromFile("users.yml")
-	if err != nil {
-		log.Println(err)
-		userDB = new(user.UserDB)
-	}
+		userDB, err = user.LoadFromFile("users.yml")
+		if err != nil {
+			log.Println(err)
+			userDB = new(user.UserDB)
+		}
+	*/
 	userDB.Path = "users.yml"
 }
 
 func main() {
 	flag.Parse()
+	if userName == "" || userPasswd == "" {
+		userDB.Save("")
+		return
+	}
 	// generate key
 	keys, err := minilock.GenerateKey(userName, userPasswd)
 	if err != nil {
@@ -39,25 +43,26 @@ func main() {
 		panic(err)
 	}
 	log.Printf("EncodeID: %s\n", encodeID)
-	err = userDB.Add(userName, userPasswd, encodeID)
+	err = userDB.Add(userName, encodeID)
 	if err != nil {
 		log.Println(err.Error())
 	}
 	/*
-		log.Printf("userDB (%v):\n%#v\n", &userDB, userDB)
-		err = user.SaveToFile(*userDB, "users.yml")
-		if err != nil {
-			panic(err)
-		}
-		err = userDB.Save("users.yml")
-		if err != nil {
-			panic(err)
+			log.Printf("userDB (%v):\n%#v\n", &userDB, userDB)
+			err = user.SaveToFile(*userDB, "users.yml")
+			if err != nil {
+				panic(err)
+			}
+			err = userDB.Save("users.yml")
+			if err != nil {
+				panic(err)
+			}
+
+		if userDB.Authenticate(userName, userPasswd) {
+			log.Printf("AUTHENTICATED!")
+			log.Printf("APIToken for %s: %s\n", userName, userDB.APIToken(userName))
 		}
 	*/
-
-	if userDB.Authenticate(userName, userPasswd) {
-		log.Printf("AUTHENTICATED!")
-		log.Printf("APIToken for %s: %s\n", userName, userDB.APIToken(userName))
-	}
+	log.Printf("APIToken for %s: %s\n", userName, userDB.APIToken(userName))
 
 }
