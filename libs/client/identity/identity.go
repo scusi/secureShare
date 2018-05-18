e90dc7c15e1b857a393771a5937cab30bba408ee
Florian Walther <florian.walther@posteo.de>
2018-05-18 10:30:49 +0200

    dleleted obsolete function UpdateKey - fixes Issue#2

--- libs/client/identity/identity.go
+++ libs/client/identity/identity.go
@@ -23,30 +23,3 @@ func New(name string) (id *Identity) {
 	id.Name = name
 	return
 }
-
-func (id *Identity) UpdateKey(baseURL string) (err error) {
-	if strings.HasSuffix(baseURL, "/") == false {
-		baseURL = baseURL + "/"
-	}
-	v := url.Values{}
-	v.Set("username", id.Name)
-	url := baseURL + "lookupKey?" + v.Encode()
-	resp, err := http.Get(url)
-	if err != nil {
-		return
-	}
-	if resp.StatusCode != 200 {
-		err = fmt.Errorf("StatusCode is '%d', something went wrong\n", resp.StatusCode)
-		return
-	}
-	publicKey, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return
-	}
-	if publicKey == nil {
-		err = fmt.Errorf("no public available")
-		return
-	}
-	id.PublicKey = string(publicKey)
-	return
-}
